d24dabc3d4a18a9aef4b93f301b605640ea205fa
kev87ian <kelvinian87@gmail.com>
2020-03-04 16:39:20 +0300

    Add Mailchimp

---
+++ script.js
@@ -0,0 +1,52 @@
+$(document).ready(function(){
+    $("#img1").click(function() {
+$("#text1").show();
+    $("#img1").hide();    
+    });
+}
+);
+$(document).ready(function(){
+    $("#text1").click(function(){
+        $("#text1").hide();
+        $("#img1").show()
+    });
+
+});
+
+$(document).ready(function(){
+    $("#img2").click(function() {
+$("#text2").show();
+    $("#img2").hide();    
+    });
+});
+
+$(document).ready(function(){
+    $("#text2").click(function(){
+        $("#text2").hide();
+        $("#img2").show()
+    });
+
+});
+
+$(document).ready(function(){
+    $("#img3").click(function() {
+$("#text3").show();
+    $("#img3").hide();    
+    });
+});
+
+$(document).ready(function(){
+    $("#text3").click(function(){
+        $("#text3").hide();
+        $("#img3").show()
+    });
+
+});
+// FORM JS
+function submitFormData(){
+var name = document.getElementById("name").val;
+var email = document.getElementById("email").val;
+var message = document.getElementById("message").val;
+ if (name !== "" && email !== "" && message !== "");
+    alert("Thank you," + name + " " + "We have received your message");
+}
---
+++ submitFormData.js
@@ -0,0 +1,9 @@
+// FORM JS
+function submitFormData() {
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var message = document.getElementById("message").value;
+    if (name !== "" && email !== "" && message !== "")
+        ;
+    alert("Thank you," + name + " " + "We have received your message");
+}
